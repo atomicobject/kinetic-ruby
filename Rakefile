edc0882f744c90eb4750ac2d416d4c0cd15a8050
Greg Williams <greg.williams@atomicobject.com>
2014-07-24 11:07:38 -0400

    Added logging level configuration

--- Rakefile
+++ Rakefile
@@ -1,8 +1,24 @@
 require 'rake'
 require_relative 'lib/kinetic-ruby'
 
+desc "Run example"
 task :example do
   kr = KineticRuby.new
+  kr.log_level = KineticRuby::LOG_LEVEL_INFO
+  kr.test_kinetic_proto
+end
+
+desc "Run example w/o logging"
+task :example_no_log do
+  kr = KineticRuby.new
+  kr.log_level = KineticRuby::LOG_LEVEL_NONE
+  kr.test_kinetic_proto
+end
+
+desc "Run example w/ verbose logging"
+task :example_verbose_log do
+  kr = KineticRuby.new
+  kr.log_level = KineticRuby::LOG_LEVEL_VERBOSE
   kr.test_kinetic_proto
 end
 
--- lib/kinetic-ruby.rb
+++ lib/kinetic-ruby.rb
@@ -3,9 +3,25 @@ require 'yaml'
 
 class KineticRuby
 
+  LOG_LEVELS = [
+    LOG_LEVEL_NONE = 0,
+    LOG_LEVEL_ERROR = 1,
+    LOG_LEVEL_INFO = 2,
+    LOG_LEVEL_VERBOSE = 3,
+  ]
+
   def initialize
     @message_out = nil
     @message_in = nil
+    @log_level = LOG_LEVEL_NONE
+  end
+
+  def log_level=(level)
+    if !LOG_LEVELS.include? level
+      raise "\nInvalid LOG_LEVEL specified!\nValid levels:\n\t" +
+        LOG_LEVELS.map{|l|"#{self.class}::#{l}"}.join("\n\t") + "\n\n"
+    end
+    @log_level = level
   end
 
   def encode_test_message
@@ -15,27 +31,50 @@ class KineticRuby
   def test_kinetic_proto
     msg = encode
     decode msg
+    puts
+    puts "Kinetic Protocol protobuf encode/decode test passed!"
   end
 
 private
 
+  def log_err(msg='')
+    log_message(msg, $stderr) if @log_level >= LOG_LEVEL_ERROR
+  end
+
+  def log_info(msg='')
+    log_message(msg) if @log_level >= LOG_LEVEL_INFO
+  end
+
+  alias log log_info
+
+  def log_verbose(msg='')
+    log_message(msg) if @log_level >= LOG_LEVEL_VERBOSE
+  end
+
+  def log_message(msg='', stream=$stdout)
+    $stderr.flush
+    stream.flush
+    stream.puts msg
+    stream.flush
+  end
+
   def encode
     pb = Seagate::Kinetic::Message.new
 
-    puts "\n\n"
-    puts "Seagate::Kinetic::Message - Encoding"
-    puts  "---------------------------------------"
+    log
+    log "#{pb.class} - Encoding"
+    log  "---------------------------------------"
 
-    puts "  fields:"
-    pb.fields.sort.each{|f| puts "    #{f}"}
-    puts
+    log_verbose "  fields:"
+    pb.fields.sort.each{|f| log_verbose "    #{f}"}
+    log_verbose
 
-    puts "  hmac:"
+    log_verbose "  hmac:"
     pb.hmac = "0123456789ABCDEF0123"
-    puts "    #{pb.hmac.inspect}"
-    puts
+    log_verbose "    #{pb.hmac.inspect}"
+    log_verbose
 
-    puts "  command:"
+    log "  command:"
     pb.command = Seagate::Kinetic::Message::Command.new(
       header: Seagate::Kinetic::Message::Header.new(
         clusterVersion: 0x1122334455667788,
@@ -52,35 +91,43 @@ private
           detailedMessage: "YOUCH!"
         ),
     )
-    pb.command.to_yaml.each_line{|l| puts "    #{l}"}
-    @message_out = pb
-    puts
+    pb.command.to_yaml.each_line{|l| log("    #{l}")}
+    log
 
-    puts "  encoded:"
+    @message_out = pb
+
+    log "  encoded:"
     encoded = pb.encode
-    puts
-    puts "    Inspection: #{encoded.inspect}"
-    # puts
-    # puts "    Content:"
-    # encoded.to_yaml.each_line{|l| puts "    #{l}"}
-    puts "    Length: #{encoded.length} bytes"
-    puts
+    log_verbose
+    log_verbose "    Inspection: #{encoded.inspect}"
+    log_verbose
+    log_verbose "    Content:"
+    encoded.to_yaml.each_line{|l| log_verbose "    #{l}"}
+    log_verbose
+    log "    Length: #{encoded.length} bytes"
+    log
 
     return encoded
   end
 
   def decode(buf)
-    puts
-    puts "Seagate::Kinetic::Message - Decoding"
-    puts  "-----------------------------------"
 
     @message_in = Seagate::Kinetic::Message.decode(buf)
-    puts "  command:"
-    @message_in.command.to_yaml.each_line{|l| puts "    #{l}"}
-    puts
 
-    raise "Kinetic Protocol message roundtrip FAILED!" if @message_in != @message_out
-    puts "Kinetic Protocol message roundtrip SUCCESS!"
+    log
+    log "#{@message_in.class} - Decoding"
+    log  "-----------------------------------"
+
+    log "  command:"
+    @message_in.command.to_yaml.each_line{|l| log "    #{l}"}
+    log
+
+    if @message_in != @message_out
+      log_err "Inbound/outbound messages do not match!\n\n"
+      raise "\nKinetic Protocol message roundtrip FAILED!\n\n"
+    end
+
+    log "Kinetic Protocol message roundtrip SUCCESS!"
   end
 
 end
